b756f2a7938539fa1ed4a53594b1863b204cfb14
mrbirne <busch.maurice@gmx.net>
2015-05-19 14:36:00 +0200

    validation category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
   has_and_belongs_to_many :questions
   has_many :scores
+
+  validates :name, presence: true
+  validates :answer_correct, length: { in: 2..100 }
 end
